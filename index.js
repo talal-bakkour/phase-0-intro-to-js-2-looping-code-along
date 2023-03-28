acb054566c4f0d33bab1b27db61514d1dc15b0aa
talal <ta440237@gmail.com>
2023-03-28 15:37:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < namesArray.length; i++) {
+      const message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  module.exports = { writeCards, countDown };
+  
